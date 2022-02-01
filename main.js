3c00f036f4897344bc81a095c7d75a138be50b12
Endi <endre1988@freemail.hu>
2022-02-01 21:12:40 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,86 +1,90 @@
-import GeoJSON from 'ol/format/GeoJSON';
-import VectorLayer from 'ol/layer/Vector';
-import VectorSource from 'ol/source/Vector';
-import 'ol/ol.css';
-import Map from 'ol/Map';
-import TileLayer from 'ol/layer/Tile';
-import OSM from 'ol/source/OSM';
-import View from 'ol/View';
-import Overlay from 'ol/Overlay';
-import {toLonLat} from 'ol/proj';
-import {toStringHDMS} from 'ol/coordinate';
+let styles = {
+  'Point': [new ol.style.Style({
+      image: new ol.style.Circle({
+          radius: 8,
+          fill: new ol.style.Fill({
+              color: [255, 255, 255, 0.3]
+          }),
+          stroke: new ol.style.Stroke({color: '#cb1d1d', width: 2})
+      })
+  })],
+  'LineString': [new ol.style.Style({
+      stroke: new ol.style.Stroke({
+          color: 'green',
+          width: 1
+      })
+  })],
+  'Polygon': [new ol.style.Style({
+      stroke: new ol.style.Stroke({
+          color: 'blue',
+          lineDash: [4],
+          width: 3
+      }),
+      fill: new ol.style.Fill({
+          color: 'rgba(0, 0, 255, 0.1)'
+      })
+  })],
+  'Circle': [new ol.style.Style({
+      stroke: new ol.style.Stroke({
+          color: 'red',
+          width: 2
+      }),
+      fill: new ol.style.Fill({
+          color: 'rgba(255,0,0,0.2)'
+      })
+  })]
+};
 
+const container = document.getElementById('popup');
+const content_element = document.getElementById('popup-content');
+const closer = document.getElementById('popup-closer');
 
-let geojsonlayer = new VectorLayer({
-  source: new VectorSource({
-    format: new GeoJSON(),
-    url: './OEVK_alap_jav.geojson',
-})
+let geojson_layer = new ol.layer.Vector({
+  source: new ol.source.Vector({
+    format: new ol.format.GeoJSON(),
+    url: './file.geojson',
+}),style: styleFunction
 });
-let x = new Map({
+
+const overlay = new ol.Overlay({
+  element: container,
+  autoPan: {
+    animation: {
+      duration: 250,
+    },
+  },
+});
+
+let x = new ol.Map({
     target: 'map-container',
     layers: [
-      new TileLayer({
-        source: new OSM()
+      new ol.layer.Tile({
+        source: new ol.source.OSM()
       }),
-      geojsonlayer,
-    
+      geojson_layer    
   ],
     overlays: [overlay],
-    view: new View({
-      center: ([2149201.0509001324, 5952841.644010644]),
-      zoom: 8,
-      maxZoom:15,
-      minZoom:6,
+    view: new ol.View({
+      center: ([2119201.0509001324, 6020841.644010644]),
+      zoom: 11,
+      maxZoom:13,
+      minZoom:9,
       extent: [762958.7313602014,5310108.422259997,3501412.7172102197,6576731.688082888]
     })
   });
-  const container = document.getElementById('popup');
-  const content = document.getElementById('popup-content');
-  const closer = document.getElementById('popup-closer');
 
-  const overlay = new Overlay({
-    element: container,
-    autoPan: {
-      animation: {
-        duration: 250,
-      },
-    },
-  });
-
-  closer.onclick = function () {
-    overlay.setPosition(undefined);
-    closer.blur();
-    return false;
-  };
-
-  x.on('singleclick', function (evt) {
-    const coordinate = evt.coordinate;
-    const hdms = toStringHDMS(toLonLat(coordinate));
-  
-    content.innerHTML = '<p>You clicked here:</p><code>' + hdms + '</code>';
-    overlay.setPosition(coordinate);
-  });
-
-
-
-
-/*var
-    container = document.getElementById('popup'),
-    content_element = document.getElementById('popup-content'),
-    closer = document.getElementById('popup-closer');
-
-closer.onclick = function() {
-    overlay.setPosition(undefined);
-    closer.blur();
-    return false;
+var styleFunction = function(feature, resolution) {
+    return styles[feature.getGeometry().getType()];
 };
-var overlay = new Overlay({
-  element: container,
-  autoPan: true,
-  offset: [+100, +100]
-});
-x.addOverlay(overlay);
+
+var fullscreen = new ol.control.FullScreen();
+x.addControl(fullscreen);
+  
+closer.onclick = function() {
+  overlay.setPosition(undefined);
+  closer.blur();
+  return false;
+};
 
 x.on('click', function(evt){
   var feature = x.forEachFeatureAtPixel(evt.pixel,
@@ -106,5 +110,8 @@ x.on('pointermove', function(e) {
   var pixel = x.getEventPixel(e.originalEvent);
   var hit = x.hasFeatureAtPixel(pixel);
   
-  x.getTarget().style.cursor = hit ? 'pointer' : '';
-});*/
+  x.getViewport().style.cursor = hit ? 'pointer' : '';
+});
+
+
+
