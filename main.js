03fcb76602c03f209d7fe1210a3f00ba4c2678d6
Endi <endre1988@freemail.hu>
2023-07-26 12:25:32 +0200

    Merge branch 'main' of https://github.com/endre88/teniszbudapesten

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const styles = {
-  'Point': [new ol.style.Style({
-     /* image: new ol.style.Circle({
+  Point: [
+    new ol.style.Style({
+      /* image: new ol.style.Circle({
           radius: 6,
           fill: new ol.style.Fill({
               color: [255, 255, 255, 0.8]
@@ -8,56 +9,65 @@ const styles = {
           stroke: new ol.style.Stroke({color: '#cb1d1d', width: 1})
       })*/
       image: new ol.style.Icon({
-          fill:'#1111ff',
-          anchor: [1, 1],
-          anchorXUnits: 'fraction',
-          anchorYUnits: 'fraction',
-          scale: 0.019,
-          src: '310075.svg'
-    })
-  })],
-  'LineString': [new ol.style.Style({
+        fill: "#1111ff",
+        anchor: [1, 1],
+        anchorXUnits: "fraction",
+        anchorYUnits: "fraction",
+        scale: 0.019,
+        src: "310075.svg",
+      }),
+    }),
+  ],
+  LineString: [
+    new ol.style.Style({
       stroke: new ol.style.Stroke({
-          color: '#fff',
-          width: 41
-      })
-  })],
-  'Polygon': [new ol.style.Style({
+        color: "#fff",
+        width: 41,
+      }),
+    }),
+  ],
+  Polygon: [
+    new ol.style.Style({
       stroke: new ol.style.Stroke({
-          color: 'blue',
-          lineDash: [4],
-          width: 4
+        color: "blue",
+        lineDash: [4],
+        width: 4,
       }),
       fill: new ol.style.Fill({
-          color: 'rgba(0, 0, 255, 0.1)'
-      })
-  })],
-  'Circle': [new ol.style.Style({
+        color: "rgba(0, 0, 255, 0.1)",
+      }),
+    }),
+  ],
+  Circle: [
+    new ol.style.Style({
       stroke: new ol.style.Stroke({
-          color: 'red',
-          width: 2
+        color: "red",
+        width: 2,
       }),
       fill: new ol.style.Fill({
-          color: 'rgba(255,0,0,0.8)'
-      })
-  })]
+        color: "rgba(255,0,0,0.8)",
+      }),
+    }),
+  ],
 };
 
-const container = document.getElementById('popup');
-const content_element = document.getElementById('popup-content');
-const closer = document.getElementById('popup-closer');
+const container = document.getElementById("popup");
+const content_element = document.getElementById("popup-content");
+const closer = document.getElementById("popup-closer");
 
-const styleFunction = function(feature, resolution) {
-  if (feature.get('name') && feature.get('address')) { //ha false a név  mező tehát nincs kitöltve akkor nem töltődik be a geometria egyáltalán
-  return styles[feature.getGeometry().getType()];}
+const styleFunction = function (feature, resolution) {
+  if (feature.get("name") && feature.get("address")) {
+    //ha false a név  mező tehát nincs kitöltve akkor nem töltődik be a geometria egyáltalán
+    return styles[feature.getGeometry().getType()];
+  }
 };
 
 const geojson_layer = new ol.layer.Vector({
   source: new ol.source.Vector({
     format: new ol.format.GeoJSON(),
-    url: './Teniszpalyak.geojson'
+    url: "./Teniszpalyak.geojson",
   }),
-  style: styleFunction
+  style: styleFunction,
 });
 
 const overlay = new ol.Overlay({
@@ -70,70 +80,113 @@ const overlay = new ol.Overlay({
 });
 
 const map = new ol.Map({
-    target: 'map-container',
-    layers: [
-      new ol.layer.Tile({
-        source: new ol.source.OSM()
-      }),
-      geojson_layer    
-  ],
-    overlays: [overlay],
-    interactions: ol.interaction.defaults({
-      onFocusOnly:true,
-      doubleClickZoom: true,
-      dragPan: true
+  target: "map-container",
+  layers: [
+    new ol.layer.Tile({
+      source: new ol.source.OSM(),
     }),
-    view: new ol.View({
-      center: ([2129201.0509001324, 6025841.644010644]),
-      zoom: 12,
-      maxZoom:18,
-      minZoom:10,
-      extent: [762958.7313602014,5310108.422259997,3501412.7172102197,6576731.688082888]
-    })
-  });
-
-
+    geojson_layer,
+  ],
+  overlays: [overlay],
+  interactions: ol.interaction.defaults({
+    onFocusOnly: true,
+    doubleClickZoom: true,
+    dragPan: true,
+  }),
+  view: new ol.View({
+    center: [2129201.0509001324, 6025841.644010644],
+    zoom: 12,
+    maxZoom: 18,
+    minZoom: 10,
+    extent: [
+      762958.7313602014, 5310108.422259997, 3501412.7172102197,
+      6576731.688082888,
+    ],
+  }),
+});
 
 const fullscreen = new ol.control.FullScreen();
 map.addControl(fullscreen);
-  
 
-closer.onclick = function() {
+closer.onclick = function () {
   overlay.setPosition(undefined);
   container.blur();
   return false;
 };
 
-
-map.on('click', function(evt){
+map.on("click", function (evt) {
   //document.body.webkitRequestFullscreen();
-  let feature = map.forEachFeatureAtPixel(evt.pixel,
-    function(feature, layer) {
-      return feature;
-    });
+  let feature = map.forEachFeatureAtPixel(evt.pixel, function (feature, layer) {
+    return feature;
+  });
   if (feature) {
-      let geometry = feature.getGeometry();
-      let coord = geometry.getCoordinates();
-      let content = ' <a target="_blank" href=' + feature.get('website') +'><h3>' + feature.get('name') + '</h3></a>';
-      content += '<h5>' + '<p class="data-label">Pálya árak: </p>'+ '<p class="data">'+ feature.get('description') + '</p></h5>';
-      content += '<h5>' + '<p class="data-label">Telefonszám: </p>'+  '<p class="data">'+feature.get('phone') + '</p></h5>';
-      content += '<h5>' + '<p class="data-label">Nyitvatartási idő: </p>'+  '<p class="data">'+feature.get('opening_hours') + '</p></h5>';
-      content += '<h5>' + '<p class="data-label">Email:  </p>'+  '<p class="data">'+feature.get('email') + '</p></h5>';
-      content += '<h5>' + '<p class="data-label">Város: </p>'+  '<p class="data">'+feature.get('city') + '</p></h5>';
-      content += '<h5>' + '<p class="data-label">Cím: </p>'+  '<p class="data">'+feature.get('address') + '</p></h5>';
-      content += '<h5>' + '<p class="data-label">Pálya típusa: </p>'+  '<p class="data">'+feature.get('surface') + '</p></h5>';
-      content += '<h5>' + '<p class="data-label">Pályák darabszáma: </p>'+  '<p class="data">'+feature.get('count') + '</p></h5>';
-      content_element.innerHTML = content;
-      overlay.setPosition(coord);}
-      else {
-        overlay.setPosition(undefined);
-        closer.blur();
-        return false;
-      }
+    let geometry = feature.getGeometry();
+    let coord = geometry.getCoordinates();
+    let content =
+      ' <a target="_blank" href=' +
+      feature.get("website") +
+      "><h3>" +
+      feature.get("name") +
+      "</h3></a>";
+    content +=
+      "<h5>" +
+      '<p class="data-label">Pálya árak: </p>' +
+      '<p class="data">' +
+      feature.get("description") +
+      "</p></h5>";
+    content +=
+      "<h5>" +
+      '<p class="data-label">Telefonszám: </p>' +
+      '<p class="data">' +
+      feature.get("phone") +
+      "</p></h5>";
+    content +=
+      "<h5>" +
+      '<p class="data-label">Nyitvatartási idő: </p>' +
+      '<p class="data">' +
+      feature.get("opening_hours") +
+      "</p></h5>";
+    content +=
+      "<h5>" +
+      '<p class="data-label">Email:  </p>' +
+      '<p class="data">' +
+      feature.get("email") +
+      "</p></h5>";
+    content +=
+      "<h5>" +
+      '<p class="data-label">Város: </p>' +
+      '<p class="data">' +
+      feature.get("city") +
+      "</p></h5>";
+    content +=
+      "<h5>" +
+      '<p class="data-label">Cím: </p>' +
+      '<p class="data">' +
+      feature.get("address") +
+      "</p></h5>";
+    content +=
+      "<h5>" +
+      '<p class="data-label">Pálya típusa: </p>' +
+      '<p class="data">' +
+      feature.get("surface") +
+      "</p></h5>";
+    content +=
+      "<h5>" +
+      '<p class="data-label">Pályák darabszáma: </p>' +
+      '<p class="data">' +
+      feature.get("count") +
+      "</p></h5>";
+    content_element.innerHTML = content;
+    overlay.setPosition(coord);
+  } else {
+    overlay.setPosition(undefined);
+    closer.blur();
+    return false;
+  }
 });
-map.on('pointermove', function(e) {
+map.on("pointermove", function (e) {
   if (e.dragging) return;
   let pixel = map.getEventPixel(e.originalEvent);
   let hit = map.hasFeatureAtPixel(pixel);
-  map.getViewport().style.cursor = hit ? 'pointer' : '';
+  map.getViewport().style.cursor = hit ? "pointer" : "";
 });
